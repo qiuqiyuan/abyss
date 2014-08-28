43b2cbcf4377b9ed71ea43137e00954712a8843a
Qiyuan Qiu <qiuqiyuan@gmail.com>
2014-08-28 19:50:30 -0400

    test sync step, add stage NAS_ERODE_COMPLETE

--- Parallel/NetworkSequenceCollection.cpp
+++ Parallel/NetworkSequenceCollection.cpp
@@ -173,7 +173,21 @@ void NetworkSequenceCollection::run()
 				m_comm.reduce(m_data.cleanup());
 				m_comm.barrier();
 				SetState(NAS_WAITING);                                
-
+#ifndef DEBUG_QQY_ENABLE
+                                //numSendPackets = m_comm.getNumSendPackets();
+                                numSendMessages = m_comm.getNumSendMessages();
+                                numSendBytes = m_comm.getNumSendBytes();
+                                //numRecvPackets = m_comm.getNumRecvPackets();
+                                numRecvMessages = m_comm.getNumRecvMessages();
+                                numRecvBytes = m_comm.getNumRecvBytes();
+                                
+                                //m_comm.gather(NULL, numSendPackets);
+                                m_comm.gather(NULL, numSendMessages);
+                                m_comm.gather(NULL, numSendBytes);
+                                //m_comm.gather(NULL, numRecvPackets);
+                                m_comm.gather(NULL, numRecvMessages);
+                                m_comm.gather(NULL, numRecvBytes);                                
+#endif
 				break;
                         }
 			case NAS_TRIM:
@@ -337,7 +351,6 @@ size_t NetworkSequenceCollection::controlErode()
 	assert(removed == numEroded);
 	(void)removed;
 	SetState(NAS_WAITING);
-
 	return numEroded;
 }
 
@@ -582,6 +595,21 @@ void NetworkSequenceCollection::runControl()
 				cout << "Eroding tips...\n";
 				controlErode();
 				SetState(NAS_TRIM);                                
+#ifndef DEBUG_QQY_ENABLE
+                                //numSendPackets = m_comm.getNumSendPackets();
+                                numSendMessages = m_comm.getNumSendMessages();
+                                numSendBytes = m_comm.getNumSendBytes();
+                                //numRecvPackets = m_comm.getNumRecvPackets();
+                                numRecvMessages = m_comm.getNumRecvMessages();
+                                numRecvBytes = m_comm.getNumRecvBytes();
+                                
+                                //outputCounter(qqy_m_numSendPackets_array, numSendPackets, "NAS_ERODE Send Packets:");
+                                outputCounter(qqy_m_numSendMessages_array, numSendMessages, "NAS_ERODE Send Messages:");
+                                outputCounter(qqy_m_numSendBytes_array, numSendBytes, "NAS_ERODE Send Bytes:");
+                                //outputCounter(qqy_m_numRecvPackets_array, numRecvPackets, "NAS_ERODE Recv Packets:");
+                                outputCounter(qqy_m_numRecvMessages_array, numRecvMessages, "NAS_ERODE Recv Messages:");
+                                outputCounter(qqy_m_numRecvBytes_array, numRecvBytes, "NAS_ERODE Recv Bytes:");
+#endif                                
 				delete rtimer;
 				break;
 			}
